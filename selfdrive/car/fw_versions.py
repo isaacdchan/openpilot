d7b189366f0c6c407942acd4aa687cf5243c77d3
Martin Lillepuu <martin@mlp.ee>
2021-07-28 20:56:53 +0300

    Reduce fw versions timeout to 0.1, enable debug

--- selfdrive/car/fw_versions.py
+++ selfdrive/car/fw_versions.py
@@ -363,7 +363,7 @@ if __name__ == "__main__":
   '''
 
   t = time.time()
-  fw_vers = get_fw_versions(logcan, sendcan, 1, extra=extra, debug=args.debug, progress=True, timeout = 0.7)
+  fw_vers = get_fw_versions(logcan, sendcan, 1, extra=extra, debug=args.debug, progress=True, timeout = 0.1)
   _, candidates = match_fw_to_car(fw_vers)
 
   print()
